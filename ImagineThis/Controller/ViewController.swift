39b29cc8f5f8edb9780302458a2df8fbd9a85a7d
AlekseyK <aleksey.kabishau@gmail.com>
2020-11-18 23:48:18 -0500

    Initial Commit

---
+++ ImagineThis/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagineThis
+//
+//  Created by Aleksey on 1118..20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        view.backgroundColor = .systemPink
+    }
+
+}
+
