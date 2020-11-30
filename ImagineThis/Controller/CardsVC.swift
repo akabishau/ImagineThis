5cc247d0c0cbb98408688a67276fc19052534032
AlekseyK <aleksey.kabishau@gmail.com>
2020-11-29 21:23:08 -0500

    feat: place and configure back button in cards view controller

--- ImagineThis/Controller/CardsVC.swift
+++ ImagineThis/Controller/CardsVC.swift
@@ -11,17 +11,20 @@ class CardsVC: UIViewController {
 
     lazy var backgroundImageView = BGImageView(frame: view.bounds)
     let grassImageView = UIImageView(image: UIImage(named: "grass"))
+    let backButton = BackButton(frame: .zero)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutUI()
+        addActionToBackButton()
     }
     
     
     private func layoutUI() {
         view.addSubview(backgroundImageView)
         view.addSubview(grassImageView)
+        view.addSubview(backButton)
         
         grassImageView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -30,7 +33,30 @@ class CardsVC: UIViewController {
             grassImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             grassImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             grassImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            grassImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+            grassImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+            
+            
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            //TODO: - test the button size on diffrent devices
+            backButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4),
+            backButton.heightAnchor.constraint(equalToConstant: 50)
         ])
+    }
+    
+    
+    private func addActionToBackButton() {
+        backButton.addTarget(self, action: #selector(backButtonTapped(_:)), for: .touchUpInside)
+    }
+    
+    
+    @objc func backButtonTapped(_ sender: UIButton) {
+        if let button = sender as? BackButton {
+            button.pulsate()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
 }
