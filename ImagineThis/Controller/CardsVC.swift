a05e912d79639d244bd30e95fa7c2d6048e38455
AlekseyK <aleksey.kabishau@gmail.com>
2020-11-29 20:30:59 -0500

    feat: add static background images for Cards view controller

--- ImagineThis/Controller/CardsVC.swift
+++ ImagineThis/Controller/CardsVC.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class CardsVC: UIViewController {
 
+    lazy var backgroundImageView = BGImageView(frame: view.bounds)
+    let grassImageView = UIImageView(image: UIImage(named: "grass"))
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
+        layoutUI()
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(grassImageView)
+        
+        grassImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            grassImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            grassImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            grassImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            grassImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+        ])
     }
 }
