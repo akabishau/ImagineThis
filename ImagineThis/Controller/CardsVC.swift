03bf7a3cd202e10f9b0333da142c13b321d7a06b
AlekseyK <aleksey.kabishau@gmail.com>
2020-12-08 13:13:59 -0500

    feat: add parent deck view and one child card view to it

--- ImagineThis/Controller/CardsVC.swift
+++ ImagineThis/Controller/CardsVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class CardsVC: UIViewController {
 
     lazy var backgroundImageView = BGImageView(frame: view.bounds)
+    let cardDeckView = UIView()
     let grassImageView = UIImageView(image: UIImage(named: "grass"))
     let backButton = BackButton(frame: .zero)
     
@@ -18,11 +19,27 @@ class CardsVC: UIViewController {
         super.viewDidLoad()
         layoutUI()
         addActionToBackButton()
+        setupCards()
     }
     
+    // TODO: - make cards deck view to be child view controller
+    private func setupCards() {
+        let cardView = CardView()
+        cardDeckView.addSubview(cardView)
+        
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: cardDeckView.topAnchor, constant: 10),
+            cardView.bottomAnchor.constraint(equalTo: cardDeckView.bottomAnchor, constant: -10),
+            cardView.leadingAnchor.constraint(equalTo: cardDeckView.leadingAnchor, constant: 10),
+            cardView.trailingAnchor.constraint(equalTo: cardDeckView.trailingAnchor, constant: -10)
+        ])
+    }
     
     private func layoutUI() {
         view.addSubview(backgroundImageView)
+        view.addSubview(cardDeckView)
+        cardDeckView.translatesAutoresizingMaskIntoConstraints = false
+        cardDeckView.backgroundColor = .systemPink
         view.addSubview(grassImageView)
         view.addSubview(backButton)
         
@@ -35,6 +52,10 @@ class CardsVC: UIViewController {
             grassImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             grassImageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
             
+            cardDeckView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            cardDeckView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            cardDeckView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            cardDeckView.heightAnchor.constraint(equalToConstant: 150),
             
             backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
