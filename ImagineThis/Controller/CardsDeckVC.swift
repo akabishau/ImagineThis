70f55ff2977de68738d78b5bdc4eaa616e7b7770
AlekseyK <aleksey.kabishau@gmail.com>
2020-12-19 16:05:26 -0500

    feat: add initial set for a cards collection view

--- ImagineThis/Controller/CardsDeckVC.swift
+++ ImagineThis/Controller/CardsDeckVC.swift
@@ -9,22 +9,24 @@ import UIKit
 
 class CardsDeckVC: UIViewController {
     
-    let cardView = CardView()
+    var collectionView: UICollectionView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupCards()
+        view.backgroundColor = .systemYellow
+        configureCollectionView()
     }
     
     
-    private func setupCards() {
-        view.addSubview(cardView)
-        cardView.fillSuperView()
-        //cardView.frame = view.bounds
+    private func configureCollectionView() {
         
-//        NSLayoutConstraint.activate([
-//
-//        ])
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()) // view.bound - why it is not working?
+        print(collectionView.frame.height)
+        view.addSubview(collectionView)
+        collectionView.register(CardCell.self, forCellWithReuseIdentifier: CardCell.reuseIndentifier)
+        
+        collectionView.backgroundColor = .systemGray
+        collectionView.fillSuperView(padding: .init(top: 10, left: 10, bottom: 10, right: 10))
     }
 }
--- ImagineThis/Controller/CardsVC.swift
+++ ImagineThis/Controller/CardsVC.swift
@@ -75,7 +75,8 @@ class CardsVC: UIViewController {
     func add(childVC: UIViewController, to containerView: UIView) {
         addChild(childVC)
         containerView.addSubview(childVC.view)
-        childVC.view.frame = containerView.bounds
+        //childVC.view.frame = containerView.bounds
+        childVC.view.fillSuperView(padding: .init(top: 10, left: 10, bottom: 10, right: 10))
         childVC.didMove(toParent: self)
     }
 }
